eb343c41ed9a79582f282785cade8019ae144a3b
Vlad Vieira <vladnascimento118@gmail.com>
2025-04-18 22:26:59 -0300

    Primeiro commit do projeto

---
+++ src/test/java/br/com/ufrn/pds/projetopds/ProjetopdsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.pds.projetopds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
