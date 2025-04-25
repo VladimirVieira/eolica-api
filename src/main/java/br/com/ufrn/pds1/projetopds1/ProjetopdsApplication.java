72bf0391943def306f23b8977744a6b44c5bb320
viniciusvilelaa <ninobez@icloud.com>
2025-04-25 19:31:11 -0300

    Adicionando dependencias do MySQL e Java JPA

--- src/main/java/br/com/ufrn/pds1/projetopds1/ProjetopdsApplication.java
+++ src/main/java/br/com/ufrn/pds1/projetopds1/ProjetopdsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Projetopds1Application {
+public class ProjetopdsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Projetopds1Application.class, args);
+		SpringApplication.run(ProjetopdsApplication.class, args);
 	}
 
 }
